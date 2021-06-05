460b6c03edd01dfd729b909de97c205fd06969a6
simba@yannitech.com <simba@yannitech.com>
2021-06-05 23:42:11 +0200

    Update component

--- ppmtool/src/main/java/za/co/bigsim/domain/Project.java
+++ ppmtool/src/main/java/za/co/bigsim/domain/Project.java
@@ -38,7 +38,8 @@ public class Project {
 	@JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
    
-	@JsonFormat(pattern = "yyyy-mm-dd")
+	@JsonFormat( pattern = "yyyy-mm-dd")
+	@Column(updatable = false)
     private Date created_At;
     
 	@JsonFormat(pattern = "yyyy-mm-dd")
